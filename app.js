2d42a18f14021552eb23734b781a466a3d9f1eed
olemoz1977 <olemoz1977@gmail.com>
2026-09-13 15:44:49 +0300

    Load scenario wording refinement

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 (() => {
   const load=(src,cb)=>{const s=document.createElement('script');s.src=src;s.onload=cb||null;document.body.appendChild(s);};
-  load('app-base.js?v=20260913b',()=>load('future.js?v=20260913b'));
+  load('app-base.js?v=20260913c',()=>load('future.js?v=20260913c',()=>load('future-fix.js?v=20260913c')));
 })();
