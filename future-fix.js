dafb8bdd3aaa23ddb59febb4fda0a69f84ca2040
olemoz1977 <olemoz1977@gmail.com>
2026-09-13 15:44:40 +0300

    Refine tax wording in demographic scenario section

---
+++ future-fix.js
@@ -0,0 +1,6 @@
+(() => {
+  const body=document.querySelector('#scenarijai .findings .finding:nth-child(5) .finding-body');
+  if(body){
+    body.innerHTML='Labai automatizuota įmonė ir toliau gali mokėti pelno bei turto mokesčius, o jos veikla ir produkcija generuoti vartojimo bei kitų mokesčių pajamas. Problema yra ne robotas pats savaime, o galimas neatitikimas tarp <em>kur kuriama ekonominė vertė</em> ir <em>nuo ko surenkamos socialinės sistemos pajamos</em>.';
+  }
+})();
